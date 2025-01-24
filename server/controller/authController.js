40a971156b08f4f07ec5a561e32cca7165d625e6
Hussnain Butt <121555081+Hussnain-Butt@users.noreply.github.com>
2025-01-24 14:31:24 +0500

    auth controller updated

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -29,22 +29,27 @@ exports.register = async (req, res) => {
 
   exports.login = async (req, res) => {
     const { email, password } = req.body;
+    console.log('Request Body:', req.body);
+  
     try {
       const user = await User.findOne({ email });
       if (!user) {
+        console.log('User not found');
         return res.status(400).json({ error: 'Invalid email or password' });
       }
   
+      console.log('User Found:', user);
+  
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
+        console.log('Password mismatch');
         return res.status(400).json({ error: 'Invalid email or password' });
       }
   
       const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
         expiresIn: '1d',
       });
-  
-      // Include profilePicture in the response
+      console.log('Login successful');
       res.json({
         token,
         user: {
@@ -56,9 +61,12 @@ exports.register = async (req, res) => {
         },
       });
     } catch (err) {
+      console.error('Error logging in:', err);
       res.status(500).json({ error: 'Error logging in' });
     }
   };
+  
+  
   
   exports.getMe = async (req, res) => {
     try {
